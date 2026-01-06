063e50a1762b3c71bfd414618efe5d47c468f6f8
astone <astone@B108-TS>
2026-01-06 09:59:09 +0000

    Add project files.

---
+++ OOPCharacter12C/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace OOPCharacter12C
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player p1 = new Player();
+            p1.setHealth(100);
+        }
+    }
+    class Player
+    {
+        private int health; // properties are set to private
+
+        public void setHealth(int newHealth) // methods are set to public
+        {
+            if (newHealth > 100)
+            {
+                newHealth = 100;
+            }
+            health = newHealth;
+        }
+    }
+}
